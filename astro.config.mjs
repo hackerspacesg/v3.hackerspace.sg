2bb6bb42fe7f4d513c88bdccad6209379db687d3
Valentine C <valentine.git@me.vc.sg>
2024-09-23 03:38:34 +0800

    Add site name to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hackerspace.sg',
   integrations: [react(), mdx(), icon()],
   vite: {
     css: {
