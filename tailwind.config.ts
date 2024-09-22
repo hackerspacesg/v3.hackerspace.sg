adc593b3cef7cd24226c3058b33f6daf557fd523
Valentine C <valentine.git@me.vc.sg>
2024-09-22 11:26:00 +0800

    Add shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
 import type { Config } from 'tailwindcss';
 
 const tailwindConfig = {
-  darkMode: 'class',
+  darkMode: ['class'],
   content: {
     files: [
       './src/**/*!(*.stories|*.spec|*.test).{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     ],
   },
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      colors: {},
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default tailwindConfig;
