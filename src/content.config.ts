94eff31f9a2b65becba75e909824580053a29951
Valentine C <valentine.git@me.vc.sg>
2025-05-08 23:32:22 +0800

    Update Astro from 4.15.4 to 5.7.12, and other packages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,11 @@
 import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const IndexCarousel = defineCollection({
-  type: 'data',
+  loader: glob({
+    pattern: '*.yaml',
+    base: './src/content/IndexCarousel',
+  }),
   schema: ({ image }) =>
     z.array(
       z.object({
